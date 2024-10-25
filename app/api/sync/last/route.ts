34ae6d0018a324dab8d17c801b1db70f2de9113c
Nicolas Ritouet <nicolas@ritouet.com>
2024-10-25 22:33:27 +0200

    remove cache from API endpoints

--- app/api/sync/last/route.ts
+++ app/api/sync/last/route.ts
@@ -2,6 +2,7 @@ import { SyncService } from "@/app/services/sync.service";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const lastSyncDate = await SyncService.getLastSyncDate();
-    return NextResponse.json(lastSyncDate);
-  }
+  const lastSyncDate = await SyncService.getLastSyncDate();
+  return NextResponse.json(lastSyncDate);
+}
+export const dynamic = 'force-dynamic'
