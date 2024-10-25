f080b6baae1f6fb8b984b1077d4b294349c2dbd3
Nicolas Ritouet <nicolas@ritouet.com>
2024-10-25 19:17:16 +0200

    Ajout @vercel/speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import {
   ClerkProvider
 } from '@clerk/nextjs';
@@ -37,6 +38,7 @@ export default function RootLayout({
             <Navbar />
             {children}
             <Analytics />
+            <SpeedInsights />
             <Footer />
             <Toaster />
           </div>
