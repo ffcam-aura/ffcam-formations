d20c587d6bff3852ea2eca59f32c7b13109a4c04
Nicolas Ritouet <nicolas@ritouet.com>
2024-10-26 10:08:41 +0200

    Cron: protect sync endpoint

--- app/api/sync/route.ts
+++ app/api/sync/route.ts
@@ -1,12 +1,12 @@
 import { SyncService } from '@/app/services/sync.service';
 
-export async function GET() {
-  // const authHeader = request.headers.get('authorization');
-  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return new Response('Unauthorized', {
-  //     status: 401,
-  //   });
-  // }
+export async function GET(request: Request) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
 
   try {
     const syncResult = await SyncService.synchronize();
